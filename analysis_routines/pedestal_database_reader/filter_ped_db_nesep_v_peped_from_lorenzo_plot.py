b1943bffb76f4e588d9746b48c13baa177005f65
jsimpson <james.simpson@ukaea.uk>
2021-09-08 23:02:11 +0100

    these are needed know to produce the nesep v. peped plots made by Lorenzo.
    However some shots are missing, I had to filter on shot number and check
    the DDAs by hand to produce the reduced set of the pedestal database
    which is called filtered_2.csv
    
    note that some shots are missing from this that are included in Lorenzo
    plots
    
    They are documed on my onenote

---
+++ analysis_routines/pedestal_database_reader/filter_ped_db_nesep_v_peped_from_lorenzo_plot.py
@@ -0,0 +1,20 @@
+import analysis_routines.pedestal_database_reader.read_ped_db as read_ped_db
+import pandas as pd
+
+
+def filter():
+    dataframe = pd.read_csv('analysis_routines/pedestal_database_reader/data/filtered_2.csv',skipinitialspace=True,converters={'dda': str.strip})
+
+    dataframe.rename(columns={'Ip (MA)':'Ip'},inplace=True)
+    dataframe.rename(columns={'B (T)':'Bt'},inplace=True)
+    dataframe.rename(columns={'P_NBI (MW)':'pnbi'},inplace=True)
+    dataframe.rename(columns={'ne separatrix from fit 10^19(m^-3)':'nesep'},inplace=True)
+    dataframe.rename(columns={'pe ped height (kPa)':'peped'},inplace=True)
+    dataframe.rename(columns={'ne ped height 10^19(m^-3)':'neped'},inplace=True)
+    dataframe.rename(columns={'Te ped height (keV)':'teped'},inplace=True)
+    dataframe.rename(columns={'average triangularity':'triangularity'},inplace=True)
+    dataframe.rename(columns={'P_TOT=PNBI+Pohm+PICRH-Pshi (MW)':'ptot'},inplace=True)
+    dataframe.rename(columns={'Te pedestal width Rmid (cm)':'deltate_cm'},inplace=True)
+    dataframe.rename(columns={'Ne pedestal width Rmid (cm)':'deltane_cm'},inplace=True)
+
+    return dataframe
